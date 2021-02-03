675da5836842f69a9007160ea5dbb441a52df769
Vinit <vinitkothari28@gmail.com>
2021-02-02 19:48:06 -0500

    faster sol

--- src/findLucky.java
+++ src/findLucky.java
@@ -4,34 +4,24 @@ import java.util.HashMap;
 public class findLucky {
 	
 	public int findLucky(int[] arr) {
-        Arrays.sort(arr);
-        if(arr[0]==arr[arr.length-1]){
-            if(arr.length==arr[0]){
-                return arr[0];
-            }
-            return -1;
-        }
-        int returner=-1;
-        
-        HashMap<Integer,Integer>mymap= new HashMap<Integer,Integer>();
-          for(int i=0;i<arr.length;i++){
-           if(mymap.get(arr[i])==null){
-               mymap.put(arr[i],1);
-           }
-           else{
-               mymap.put(arr[i],mymap.get(arr[i])+1);
-           }
-       }
-        
-        for(int x:mymap.keySet()){
-            if(mymap.get(x)==x){
-                returner=mymap.get(x);
-            }
-        }
-       
-     return returner;
-        
-    }
-}
-
+		  Arrays.sort(arr);
+		    
+	        
+	        for(int i=arr.length-1;i>=0;){
+	            for(int j=0;j<=i;j++){
+	                
+	                 if(arr[i]==arr[j]){
+	                    if(arr[i]==i-j+1){
+	                        return arr[i];
+	                    }
+	                           else{
+	                        i=j-1;
+	                        break;
+	                    }
+	                }
+	            }
+	        }
+	        return -1;
+	    }
+	}
 
